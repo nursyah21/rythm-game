96cc255e6e645dcd31bb0a85e4d6e60d360c9a3a
nursyah21 <nursyahjaya@gmail.com>
2025-07-02 22:02:59 +0800

    data musics

---
+++ src/assets/data.ts
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        title: "rokudenashi - ambigious",
+        song: "rokudenashi-ambigious.mp4",
+        art: "rokudenashi-ambigious.webp",
+        lyrics: ""
+    },
+    {
+        id: 2,
+        title: "rokudenashi - deep down",
+        song: "rokudenashi-deep-down.mp4",
+        art: "rokudenashi-deep-down.webp",
+        lyrics: ""
+    },
+    {
+        id: 3,
+        title: "rokudenashi - sincerely yours",
+        song: "rokudenashi-sincerely-yours.mp4",
+        art: "rokudenashi-sincerely-yours.webp",
+        lyrics: ""
+    },
+    {
+        id: 4,
+        title: "rokudenashi - flame of love",
+        song: "rokudenashi-flame-of-love.mp4",
+        art: "rokudenashi-flame-of-love.webp",
+        lyrics: ""
+    },
+    {
+        id: 5,
+        title: "rokudenashi - one voice",
+        song: "rokudenashi-one-voice.mp4",
+        art: "rokudenashi-one-voice.webp",
+        lyrics: ""
+    },
+]
